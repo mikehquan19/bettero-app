8f815a4078368de34955aa105fb2db5f4922ee3c
mikehquan19 <hoangphucquan19@gmail.com>
2024-08-29 11:20:03 -0500

    modify build

--- reactproject/first-react-app/vite.config.js
+++ reactproject/first-react-app/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir: "build",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests")
+    }
   }
 })
