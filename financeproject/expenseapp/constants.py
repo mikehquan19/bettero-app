481fa1c310253108f0cc5cd34f17013770cf15e6
mikehquan19 <hoangphucquan19@gmail.com>
2025-06-15 14:02:40 -0500

    Create file to store all the constants of the project

---
+++ financeproject/expenseapp/constants.py
@@ -0,0 +1,13 @@
+CATEGORY_DICT = {
+    "Income": "INCOME",
+    "Housing": "HOUSING",
+    "Automobile": "AUTO",
+    "Medical": "MEDICAL",
+    "Subscription": "SUBSCRIPTION",
+    "Grocery": "GROCERY", 
+    "Dining": "FOOD & DRINK", 
+    "Shopping": "SHOPPING", 
+    "Gas": "GAS",
+    "Others": "OTHERS",
+}
+
