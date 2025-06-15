3b626cebdd799743173b0a9c5a8eb5baf04e21f0
mikehquan19 <hoangphucquan19@gmail.com>
2025-06-15 00:05:18 -0500

    Remove unecessary comments

--- financeproject/financeproject/celery.py
+++ financeproject/financeproject/celery.py
@@ -6,11 +6,6 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'financeproject.settings')
 
 app = Celery('financeproject')
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object(f'django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
