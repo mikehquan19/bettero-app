88dc57d93f58c5d28d31fcb2ad7516bbfc086aca
mikehquan19 <hoangphucquan19@gmail.com>
2025-06-15 00:00:30 -0500

    Update logic to access financial from yfinance dataframe

--- financeproject/expenseapp/finance/stock_finance.py
+++ financeproject/expenseapp/finance/stock_finance.py
@@ -1,19 +1,16 @@
 from decimal import Decimal
+import decimal
+from os import symlink
 from typing import Dict
 from datetime import date, timedelta
 import yfinance as yf
 
 # get the first date of last month and current date 
 def get_first_and_last_dates(): 
-    current_date = date.today() # the last date (which is today)
+    current_date = date.today() # the last date, which is today
 
-    """
-    the first date (which is first date of last month)
-    month and year of the last date 
-    """
-    curr_month, curr_year = current_date.month, current_date.year
-    prev_month, prev_year = curr_month - 1, curr_year
-    
+    # the first date (which is first date of last month), month and year of the last date 
+    prev_month, prev_year = current_date.month - 1, current_date.year
     if prev_month < 0: 
         prev_month, prev_year = 12, prev_year - 1
 
@@ -41,55 +38,53 @@ def load_stock_data(symbol: str) -> Dict:
     first_date, last_date = get_first_and_last_dates()
 
     # Load data of the stock's info 
-    recent_data = yf.download(
-        [symbol], start=to_string(first_date), end=to_string(last_date)
-    )
+    recent_data = yf.download(symbol, start=to_string(first_date), end=to_string(last_date))
 
     # current info of the stock 
-    custom_data = {
-        "current_close": round(Decimal(recent_data["Adj Close"].iloc[-1]), 2),
-        "previous_close": round(Decimal(recent_data["Adj Close"].iloc[-2]), 2), 
-        "open": round(Decimal(recent_data["Open"].iloc[-1]), 2), 
-        "high": round(Decimal(recent_data["High"].iloc[-1]), 2), 
-        "low": round(Decimal(recent_data["Low"].iloc[-1]), 2), 
-        "volume": int(recent_data["Volume"].iloc[-1]),
-    }
+    stock_data = {}
+    for field in ["current_close", "previous_close", "open", "high", "low", "volume"]: 
+        frame_col = field.split('_')[-1].capitalize()
+        index = -2 if field == "previous_close" else -1
+        value = recent_data[frame_col][symbol].iloc[index] 
+        stock_data[field] = round(int(value), 2) if field == "volume" else round(Decimal(value), 2)
 
     # The price of the stock over the past
-    custom_data["price_data"] = []
+    stock_data["price_data"] = []
     current_date = first_date 
     while current_date < last_date: 
         try: 
-            given_date_price = recent_data["Adj Close"][to_string(current_date)]
+            given_date_price = recent_data["Close"][symbol][to_string(current_date)]
             item_data = {
                 "date": current_date, 
                 "given_date_close": float(round(given_date_price, 2))
             }
-            custom_data["price_data"].append(item_data)
-        # The key error means that the price of stock on that date doesn't exist
+            stock_data["price_data"].append(item_data)
         except KeyError: 
+            # The key error means that the price of stock on that date doesn't exist
             pass
+        
         current_date += timedelta(days=1) # Increment the date
 
     # The date the price of the stock was updated 
-    last_updated_date = custom_data["price_data"][-1]["date"]
-    custom_data["last_updated_date"] = last_updated_date
-    return custom_data
+    last_updated_date = stock_data["price_data"][-1]["date"]
+    stock_data["last_updated_date"] = last_updated_date
+
+    return stock_data
 
 
 # Update the info the stock, and add new record of the stock price 
 def update_stock_data(symbol: str) -> Dict: 
     previous_date = to_string(date.today() - timedelta(days=1))
     today = to_string(date.today())
-    
+
     # Return the Panda data frame 
     updated_data = yf.download(symbol, start=previous_date, end=today)
-    
-    custom_data = {
-        "new_close": round(Decimal(updated_data["Adj Close"].iloc[0]), 2), 
-        "new_open": round(Decimal(updated_data["Open"].iloc[0]), 2), 
-        "new_high": round(Decimal(updated_data["High"].iloc[0]), 2), 
-        "new_low": round(Decimal(updated_data["Low"].iloc[0]), 2), 
-        "new_volume": int(updated_data["Volume"].iloc[0])
-    }
+
+    custom_data = {}
+    for field in ["new_close", "new_open", "new_high", "new_low", "new_volume"]: 
+        frame_col = field.split('_')[-1].capitalize()
+        value = updated_data[frame_col][symbol].iloc[0]
+        value = int(value) if field == "new_volume" else Decimal(value)
+        custom_data[field] = round(value, 2)
+
     return custom_data
