19b633380468f53fe157fab33a21a54d9fed31fd
mikehquan19 <hoangphucquan19@gmail.com>
2025-08-10 01:00:41 -0500

    Add validation for transaction and budget plan forms

--- bettero-front-end/src/components/Forms/AddForms/Validation.ts
+++ bettero-front-end/src/components/Forms/AddForms/Validation.ts
@@ -31,6 +31,29 @@ export const ADD_ACCOUNT_VALIDATION = {
     }
 };
 
+export const ADD_TRANSACTION_VALIDATION = {
+    // validation on the description field 
+    description: {
+        required: "Description is required",
+        maxLength: {
+            value: 200,
+            message: "Description must be less than 200 characters",
+        }
+    },
+    // validation on the amount field
+    amount: {
+        required: "Amount is required",
+        min: {
+            value: 0.01,
+            message: "Amount must be at least 0.01",
+        },
+        maxLength: {
+            value: 10,
+            message: "Amount must be less than 10 digits "
+        }
+    }
+}
+
 export const ADD_BILL_VALIDATION = {
     description: {
         required: "*Description is required",
@@ -65,4 +88,40 @@ export const ADD_STOCK_VALIDATTION = {
             message: "*Shares must be less than 1000000",
         }
     }
+}
+
+/**
+ * Get the validation for the form to add or update budget plans in runtime
+ */
+export function getAddBudgetPlanValidation() {
+    const categories = ["Total", "Housing", "Automobile", "Medical", "Subscription", "Grocery", "Dining", "Shopping", "Gas", "Others"];
+    const registerOptions = {
+        recurringIncome: {
+            required: "*Income is required",
+            min: {
+                value: 0.01,
+                message: "*Income must be greater than 0",
+            },
+            maxLength: {
+                value: 12,
+                message: "*Income only has at most 12 digits",
+            }
+        },
+    } as Record<string, any>;
+
+    categories.forEach(category => {
+        const field = category === 'Total' ? 'portionForExpense' : category;
+        registerOptions[field] = {
+            required: `*${category} required`,
+            min: {
+                value: 0,
+                message: "*Percentage must be greater than or equal 0",
+            },
+            max: {
+                value: 100,
+                message: "*Percentage must be less than or equal 100",
+            }
+        }
+    });
+    return registerOptions;
 }
